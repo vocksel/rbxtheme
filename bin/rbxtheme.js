de05ed9ea6a59970788e322a69b69ac7c1f65122
David Minnerly <dminnerly@roblox.com>
2021-07-02 11:07:47 -0400

    Show the full command by default

--- bin/rbxtheme.js
+++ bin/rbxtheme.js
@@ -10,22 +10,21 @@ const exit = (message) => {
     console.log('Usage: rbxtheme path/to/theme.json')
 }   
 
-const copiedSuccessfully = () => {
-    console.log('Theme copied to clipboard. Paste it into the command bar in Studio to set your Script Editor theme.')
-}
 program
     .version('1.0.0')
-    .argument('<themeFile>', `Path to the theme's json file`)
-    .option('--show-command', `Log the command to the console. Useful if copying to clipboard doesn't work`)
+    .argument('<themeFile>', `Path to the theme's json file. These can be found in ~/.vscode/extensions`)
+    .option('-c, --clipboard', 'Copy the generated command to the clipboard automatically')
     .action(async (themeFile, options) => {
         if (fs.existsSync(themeFile)) {
             const command = convert(themeFile)
             const minified = command.replace(/\s+|[\t\r\n]/gm, ' ')
             
+            if (options.clipboard) {
             await clipboardy.write(minified)
-            copiedSuccessfully()
-            
-            if (options.showCommand) {
+                console.log('Theme copied to clipboard. Paste it into the command bar in Studio to set your ' +
+                    'Script Editor theme.')
+            } else {
+                console.log('Copy/paste this command into the command bar in Studio to set your Script Editor theme')
                 console.log('\n' + minified)
             }
         } else {
