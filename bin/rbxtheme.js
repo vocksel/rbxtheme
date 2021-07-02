7ab63caece460b78be10eaeb5ba90a5828a82bd3
David Minnerly <dminnerly@roblox.com>
2021-07-02 11:08:07 -0400

    Show help on error

--- bin/rbxtheme.js
+++ bin/rbxtheme.js
@@ -5,22 +5,18 @@ import clipboardy from 'clipboardy'
 import fs from 'fs'
 import convert from '../index.js'
 
-const exit = (message) => {
-    console.error(chalk.red(message))
-    console.log('Usage: rbxtheme path/to/theme.json')
-}   
-
 program
     .version('1.0.0')
     .argument('<themeFile>', `Path to the theme's json file. These can be found in ~/.vscode/extensions`)
     .option('-c, --clipboard', 'Copy the generated command to the clipboard automatically')
+    .showHelpAfterError()
     .action(async (themeFile, options) => {
         if (fs.existsSync(themeFile)) {
             const command = convert(themeFile)
             const minified = command.replace(/\s+|[\t\r\n]/gm, ' ')
-            
+
             if (options.clipboard) {
-            await clipboardy.write(minified)
+                await clipboardy.write(minified)
                 console.log('Theme copied to clipboard. Paste it into the command bar in Studio to set your ' +
                     'Script Editor theme.')
             } else {
@@ -28,7 +24,8 @@ program
                 console.log('\n' + minified)
             }
         } else {
-            exit(`Could not find a theme file at the path ${themeFile}`)
+            console.log(chalk.red(`Could not find a theme file at the path '${themeFile}'`))
+            command.help()
         }
     })
     .parse(process.argv)
