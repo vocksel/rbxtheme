e471773a2ce410d3dbb881ef9f7d173d039f67af
David Minnerly <dminnerly@roblox.com>
2021-07-02 13:02:07 -0400

    Improve theme discoverability

--- bin/rbxtheme.js
+++ bin/rbxtheme.js
@@ -2,20 +2,91 @@
 import { program } from 'commander'
 import chalk from 'chalk'
 import clipboardy from 'clipboardy'
+import os from 'os'
 import fs from 'fs'
+import { readdir, stat, readFile } from 'fs/promises'
+import JSON5 from 'json5'
+import path from 'path'
 import convert from '../index.js'
 
 const ISSUES_URL = 'https://github.com/vocksel/roblox-theme-converter/issues'
 
+const getAvailableThemes = async () => {
+    const extensionsPath = path.join(os.homedir(), '.vscode/extensions/')
+    const extensions = await readdir(extensionsPath)
+
+    const themes = []
+
+    for (const extension of extensions) {
+        const extensionPath = path.join(extensionsPath, extension)
+        const stats = await stat(extensionPath)
+
+        if (stats.isDirectory()) {
+            const items = await readdir(extensionPath)
+            const themesFolder = items.find(item => item === 'themes') 
+
+            if (themesFolder) {
+                const themesPath = path.join(extensionPath, 'themes')
+                const themeFiles = await readdir(themesPath)
+
+                for (const themeFile of themeFiles) {
+                    if (themeFile.endsWith('.json')) {
+                        const themePath = path.join(themesPath, themeFile)
+                        
+                        let theme
+                        try {
+                            theme = JSON5.parse(await readFile(themePath))
+                        } catch {
+                            // Ignore
+                        }
+
+                        if (theme?.name) {
+                            themes.push({
+                                name: theme.name,
+                                path: themePath,
+                            })
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return themes
+}
+
+const getThemeFromName = async (themeName) => {
+    // Check if the theme exists in the cwd first so that we can easily test any theme file.
+    const relativeTheme = fs.existsSync(themeName)
+    
+    if (relativeTheme) {
+        return themeName
+    } else {
+        const themes = await getAvailableThemes()
+        const theme = themes.find(theme => themeName === theme.name)
+
+        if (theme) {
+            return theme.path
+        }
+    }
+}
+
 program
     .version('1.0.0')
-    .argument('<themeFile>', `Path to the theme's json file. These can be found in ~/.vscode/extensions`)
+    .showHelpAfterError()
+
+program
+    .command('convert', { isDefault: true })
+    .description('Converts one of the available VSCode themes into a command you can run from Studio to set your '
+        + 'Script Editor colors')
+    .argument('<theme>', `Name of the theme file to convert. Run 'rbxtheme list' for a list of themes you can use`)
     .option('-c, --clipboard', 'Copy the generated command to the clipboard automatically')
     .option('-e, --expanded', 'Log the Studio command in its non-minified form')
-    .showHelpAfterError()
-    .action(async (themeFile, options) => {
-        if (fs.existsSync(themeFile)) {
-            const [command, missingColors] = convert(themeFile)
+    .action(async (theme, options, command) => {
+        const themePath = await getThemeFromName(theme)
+
+        if (fs.existsSync(themePath)) {
+            const [command, missingColors] = convert(themePath)
             const minified = command.replace(/\s+|[\t\r\n]/gm, ' ')
 
             if (options.clipboard) {
@@ -40,8 +111,22 @@ program
                     + `you are converting.`))
             }
         } else {
-            console.log(chalk.red(`Could not find a theme file at the path '${themeFile}'`))
+            console.log(chalk.red(`Could not find a theme file named '${theme}'`))
             command.help()
         }
     })
-    .parse(process.argv)
+
+program
+    .command('list')
+    .description('Lists the names of the available themes. Use these names with the `convert` command to generate '
+        + 'Studio themes.')
+    .action(async () => {
+        const themes = await getAvailableThemes()
+
+        console.log('Available themes:')
+        for (const {name} of themes) {
+            console.log(`- ${name}`)
+        }
+    })
+
+program.parse(process.argv)
