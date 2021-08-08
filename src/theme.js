bb38bc70fced84a762068def60a864ea1f03e368
david vocksel <voxeldavid@gmail.com>
2021-08-08 08:48:31 -0700

    Add support for One Dark Pro (#8)

--- src/theme.js
+++ src/theme.js
@@ -14,14 +14,19 @@ const getScopeColors = (theme) => {
         const color = token.settings.foreground
 
         if (color) {
-            // The token scope can either be an array of strings, or a string. This
-            // handles both cases.
+            // The token scope can either be an array of strings or a string. In
+            // some older themes, the scope can be a comma separated string
+            // denoting the list of scopes, so we have to handle both cases
+            // where the scope string is either a single scope, or many.
+            let scopes = []
             if (Array.isArray(token.scope)) {
-                for (const scope of token.scope) {
-                    colors[scope] = color
-                }
+                scopes = token.scope
             } else {
-                colors[token.scope] = color
+                scopes = token.scope.split(',')
+            }
+
+            for (const scope of scopes) {
+                colors[scope] = color
             }
         }
     }
